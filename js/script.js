c1336d11d28a7897ea79e1fddb629e5812faa692
harsh dhabai <harshdhabai80530@hmail.com>
2022-12-27 23:15:30 +0530

    another1 website

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert("Wlcome to the click shop");
+}
+
+function myclick(){
+    alert("you have successfully registered :- Now login your details");
+}
+
+function otpclick(){
+    alert("otp successfully sent");
+}
+
+function forgotclick(){
+    alert("reset successfully now login");
+}
